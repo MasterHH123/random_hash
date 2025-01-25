04f420d25622c962ef070e9566606bdcea5f88f9
Horacio H <hernandezh4@hotmail.com>
2025-01-24 19:26:24 -0800

    random_hash function, test and workflow were added.

--- hello.py
+++ hello.py
@@ -1,4 +1,13 @@
-def add(x,y):
-    return x + y
+import hashlib, os
 
-print(add(1,1))
+def random_hash():
+    n = 1000
+
+    for _ in range(n):
+        hash = hashlib.md5(os.urandom(32)).hexdigest()
+        if hash[0] == '0' and hash[1] == '0':
+            #found = True
+            #break
+            return hash
+    return None
+
--- test_hello.py
+++ test_hello.py
@@ -1,4 +1,8 @@
 import hello
 
-def test_add():
-    assert 2 == hello.add(1,1)
+def test_random_hash():
+    result = hello.random_hash()
+    if result:
+        assert result[0] == '0' and result[1] == '0', "Hash does not start with 00."
+    else:
+        assert result is None, "No hash should be returned if none match."
